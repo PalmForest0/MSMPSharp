6f71ae4006e3a7d485a063a848afe465cecd773d
PalmForest <realpalmforest@gmail.com>
2026-02-08 17:28:45 +1100

    Implement response receival loop

--- src/Core/MsmpClient.cs
+++ src/Core/MsmpClient.cs
@@ -2,6 +2,7 @@
 using MSMPSharp.Models.RPC;
 using MSMPSharp.Modules;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 using System.Net.WebSockets;
 using System.Text;
@@ -15,7 +16,10 @@ public class MsmpClient : IAsyncDisposable
     private readonly Uri _serverUri;
     private readonly ClientWebSocket _socket;
 
-    private int _latestRequestId = 1;
+    private int _latestRequestId = 0;
+
+    private readonly Dictionary<int, TaskCompletionSource<JsonRpcResponse>> _pendingRequests = new();
+    private readonly object _lockRequests = new();
 
     // All modules available within the client
     public PlayersModule Players { get; }
@@ -24,7 +28,7 @@ public class MsmpClient : IAsyncDisposable
     public IpBansModule IpBans { get; }
     public OperatorsModule Operators { get; }
     public ServerModule Server { get; }
-    public GameRulesModule Gamerules { get; }
+    public GameRulesModule GameRules { get; }
     public ServerSettingsModule ServerSettings { get; }
 
     public MsmpClient(string host, int port, string secret)
@@ -39,22 +43,80 @@ public class MsmpClient : IAsyncDisposable
         IpBans = new IpBansModule(this);
         Operators = new OperatorsModule(this);
         Server = new ServerModule(this);
-        Gamerules = new GameRulesModule(this);
+        GameRules = new GameRulesModule(this);
         ServerSettings = new ServerSettingsModule(this);
     }
 
     /// <summary>
     /// Connects to the Minecraft server through the websocket.
     /// </summary>
-    public async Task ConnectAsync() => await _socket.ConnectAsync(_serverUri, CancellationToken.None);
+    public async Task ConnectAsync()
+    {
+        await _socket.ConnectAsync(_serverUri, CancellationToken.None);
+
+        // Start a receive loop on a second thread
+        _ = Task.Run(ReceiveLoopAsync);
+    }
 
     /// <summary>
     /// Disconnects from the Minecraft server and disposes the websocket connection.
     /// </summary>
     public async Task DisconnectAsync()
     {
-        await _socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Success.", CancellationToken.None);
+        if (_socket.State == WebSocketState.Open)
+            await _socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Success.", CancellationToken.None);
         _socket.Dispose();
+    }
+
+    private async Task ReceiveLoopAsync()
+    {
+        while (_socket.State == WebSocketState.Open)
+        {
+            string json = await _socket.ReceiveInChunksAsync(CancellationToken.None);
+            var obj = JObject.Parse(json);
+
+            if(obj is null)
+                continue;
+
+            // If the json has an id, it is a response
+            if (obj["id"] is not null)
+                HandleResponse(obj);
+            // If the json has a method, it is a notification
+            else if (obj["method"] is not null)
+                HandleNotification(obj);
+        }
+    }
+
+    private void HandleResponse(JObject jobj)
+    {
+        var response = jobj.ToObject<JsonRpcResponse>();
+
+        if (response is null)
+            return;
+
+        // Find the task that corresponds to this response id
+        TaskCompletionSource<JsonRpcResponse>? tcs;
+        lock(_lockRequests)
+        {
+            if (!_pendingRequests.TryGetValue(response.Id, out tcs))
+                return;
+
+            _pendingRequests.Remove(response.Id);
+        }
+
+        if (response.Error is not null)
+            throw new WebSocketException($"{response.Error.Message} ({response.Error.Code})\n\"{response.Error.Data}\"");
+
+        if (response.Result is null)
+            throw new InvalidOperationException("Result is missing from the response.");
+
+        // Set the task result to the response
+        tcs.SetResult(response);
+    }
+
+    private void HandleNotification(JObject response)
+    {
+        throw new NotImplementedException();
     }
 
     /// <summary>
@@ -70,27 +132,9 @@ public class MsmpClient : IAsyncDisposable
     }
 
     /// <summary>
-    /// Receives a JSON RPC response and returns the result as the specified type.
-    /// </summary>
-    /// <typeparam name="T">The expected type of the response result.</typeparam>
-    /// <returns>The deserialized result of type <typeparamref name="T"/>.</returns>
-    /// <exception cref="InvalidOperationException">Thrown when the response is invalid or missing a result.</exception>
-    /// <exception cref="WebSocketException">Thrown when the response contains an error.</exception>
-    private async Task<T> ReceiveResponseAsync<T>()
-    {
-        string json = await _socket.ReceiveInChunksAsync(CancellationToken.None);
-        var response = JsonConvert.DeserializeObject<JsonRpcResponse>(json) ?? throw new InvalidOperationException("Invalid JSON-RPC response.");
-        if (response.Error is not null)
-            throw new WebSocketException($"{response.Error.Message} ({response.Error.Code})\n\"{response.Error.Data}\"");
-        if (response.Result is null)
-            throw new InvalidOperationException("Result is missing from the response.");
-        return response.Result.ToObject<T>()!;
-    }
-
-    /// <summary>
     /// Calls a JSON-RPC method on the Minecraft server with no parameters.
     /// </summary>
-    /// <typeparam name="T">The expected type of the response result.</typeparam>
+    /// <typeparam name="T">The expected type of the jobj result.</typeparam>
     /// <param name="method">The name of the method to call.</param>
     /// <returns>The deserialized result of type <typeparamref name="T"/>.</returns>
     public async Task<T> CallMethodAsync<T>(string method) => await CallMethodAsync<T>(method, Array.Empty<object>());
@@ -98,7 +142,7 @@ public class MsmpClient : IAsyncDisposable
     /// <summary>
     /// Calls a JSON-RPC method on the Minecraft server with the specified parameters.
     /// </summary>
-    /// <typeparam name="T">The expected type of the response result.</typeparam>
+    /// <typeparam name="T">The expected type of the jobj result.</typeparam>
     /// <param name="method">The name of the method to call.</param>
     /// <param name="parameters">The parameters to pass to the method.</param>
     /// <returns>The deserialized result of type <typeparamref name="T"/>.</returns>
@@ -108,19 +152,30 @@ public class MsmpClient : IAsyncDisposable
         {
             Method = method,
             Params = parameters,
-            Id = _latestRequestId++
+            Id = ++_latestRequestId
         });
-        return await ReceiveResponseAsync<T>();
+
+        // Create TaskCompletionSource
+        var tcs = new TaskCompletionSource<JsonRpcResponse>(TaskCreationOptions.RunContinuationsAsynchronously);
+        
+        // Add it with this request's id
+        lock (_lockRequests)
+        {
+            _pendingRequests.Add(_latestRequestId, tcs);
+        }
+
+        // Await until the task result is set by the receiver
+        var response = await tcs.Task;
+        var result = response.Result!.ToObject<T>();
+
+        if (result is null)
+            throw new InvalidOperationException($"Failed to deserialize result to type {typeof(T).Name}.");
+
+        return result;
     }
 
     /// <summary>
     /// Asynchronously disposes the client and closes the websocket connection.
     /// </summary>
-    public async ValueTask DisposeAsync()
-    {
-        if (_socket.State == WebSocketState.Open)
-            await _socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Success.", CancellationToken.None);
-
-        _socket.Dispose();
-    }
+    public async ValueTask DisposeAsync() => await DisconnectAsync();
 }
