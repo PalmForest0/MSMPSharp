8560d60af1a5d04f1d1c4d7d08e7fb91205a66d0
PalmForest <realpalmforest@gmail.com>
2026-07-03 13:43:36 +1000

    Fix potential bug in DateTime extension

--- src/Extensions/DateTimeExtensions.cs
+++ src/Extensions/DateTimeExtensions.cs
@@ -2,5 +2,12 @@
 
 internal static class DateTimeExtensions
 {
-    internal static string ToMCString(this DateTime dateTime) => dateTime.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ");
+    internal static string ToMCString(this DateTime dateTime)
+    {
+        var utc = dateTime.Kind == DateTimeKind.Unspecified
+            ? throw new ArgumentException("DateTime must have a specified kind (UTC or Local).", nameof(dateTime))
+            : dateTime.ToUniversalTime();
+
+        return utc.ToString("yyyy-MM-ddTHH:mm:ssZ");
+    }
 }
